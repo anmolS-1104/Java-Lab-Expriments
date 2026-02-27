9bba9e694615ec9aea12d7c0bc3a9dc0883b5b3a
Anmol <anmolshinde206@gmail.com>
2026-02-27 23:57:46 +0530

    Updated

--- Exp4/VectorException.java
+++ Exp4/VectorException.java
@@ -1,8 +1,8 @@
 package Exp4;
 
 public class VectorException extends Exception {
-    public VectorException(String msg){
-        super(msg);
+    public VectorException(String message){
+        super(message);
     }
     
 }
--- Exp4/VectorOperations.java
+++ Exp4/VectorOperations.java
@@ -1,43 +1,64 @@
 package Exp4;
 
 public class VectorOperations {
-    public static void main(String[] args){
-        public Vector add(Vector other) {
-            validateMatch(other);
-            double[] result = new double[dim];
-    for (int i = 0; i < dim; i++) {
-        result[i] = this.V[i] + other.V[i];
-    }
-    return new Vector(result);
-}
-public Vector subtract(Vector other) {
-    validateMatch(other);
-    double[] result = new double[dim];
-    for (int i = 0; i < dim; i++) {
-        result[i] = this.V[i] - other.V[i];
-    }
-    return new Vector(result);
-}
-public double dotProduct(Vector other) {
-    validateMatch(other);
-    double result = 0;
-    for (int i = 0; i < dim; i++) {
-        result += this.V[i] * other.V[i];
-    }
-    return result;
-}
-public void display() {
-    System.out.print("(");
-    for (int i = 0; i < dim; i++) {
-        System.out.print(V[i]);
-        if (i < dim - 1) {
-            System.out.print(", ");
+
+    // Requirement: Method to check dimensions
+    public static void validateMatch(Vector v1, Vector v2) throws VectorException {
+        if (v1.dim != v2.dim) {
+            throw new VectorException("Dimensions do not match: " + v1.dim + "D vs " + v2.dim + "D");
         }
     }
-    System.out.println(")");
-}
+
+    // Requirement: add() returns Vector
+    public static Vector add(Vector v1, Vector v2) throws VectorException {
+        validateMatch(v1, v2);
+        double[] res = new double[v1.dim];
+        for (int i = 0; i < v1.dim; i++) {
+            res[i] = v1.V[i] + v2.V[i];
+        }
+        return new Vector(res);
     }
-    
 
+    // Requirement: subtract() returns Vector
+    public static Vector subtract(Vector v1, Vector v2) throws VectorException {
+        validateMatch(v1, v2);
+        double[] res = new double[v1.dim];
+        for (int i = 0; i < v1.dim; i++) {
+            res[i] = v1.V[i] - v2.V[i];
+        }
+        return new Vector(res);
+    }
+
+    // Requirement: dotProduct() returns a single value
+    public static double dotProduct(Vector v1, Vector v2) throws VectorException {
+        validateMatch(v1, v2);
+        double res = 0;
+        for (int i = 0; i < v1.dim; i++) {
+            res += v1.V[i] * v2.V[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Vector v1 = new Vector(new double[]{10, 20, 30});
+            Vector v2 = new Vector(new double[]{5, 5, 5});
+
+            System.out.print("v1: "); v1.display();
+            System.out.print("v2: "); v2.display();
+
+            // Store in 'v' as requested
+            Vector v = add(v1, v2);
+            System.out.print("Result Addition (v): "); v.display();
+
+            v = subtract(v1, v2);
+            System.out.print("Result Subtraction (v): "); v.display();
+
+            double dot = dotProduct(v1, v2);
+            System.out.println("Result Dot Product: " + dot);
+
+        } catch (VectorException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
-
